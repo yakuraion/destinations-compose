1292c7455f619764870a15bd656cd88c05db49d5
Viachaslau Yakauleu <yakuraion@gmail.com>
2023-10-27 02:02:39 +0300

    add MavenCentral repository to publishing

--- gradle/plugins/publishing/src/main/kotlin/publishing-conventions.gradle.kts
+++ gradle/plugins/publishing/src/main/kotlin/publishing-conventions.gradle.kts
@@ -42,6 +42,19 @@ publishing {
             }
         }
     }
+
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+            credentials {
+                username = project.property("ossrhUsername") as String?
+                password = project.property("ossrhPassword") as String?
+            }
+        }
+    }
 }
 
 signing {
